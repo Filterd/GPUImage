678298c53beb505a98b43b1bc795e70ce6170cf1
Filterd <optic@Steves-MacBook-Pro.local>
2016-03-28 18:41:20 +0100

    Soft Light Mode Premultiplied

--- framework/Source/GPUImageSoftLightBlendFilter.m
+++ framework/Source/GPUImageSoftLightBlendFilter.m
@@ -14,6 +14,9 @@ NSString *const kGPUImageSoftLightBlendFragmentShaderString = SHADER_STRING
      mediump vec4 base = texture2D(inputImageTexture, textureCoordinate);
      mediump vec4 overlay = texture2D(inputImageTexture2, textureCoordinate2);
      
+    base.rgb *= base.a;
+     overlay.rgb *= overlay.a;
+     
      lowp float alphaDivisor = base.a + step(base.a, 0.0); // Protect against a divide-by-zero blacking out things in the output
      gl_FragColor = base * (overlay.a * (base / alphaDivisor) + (2.0 * overlay * (1.0 - (base / alphaDivisor)))) + overlay * (1.0 - base.a) + base * (1.0 - overlay.a);
  }
@@ -31,6 +34,9 @@ NSString *const kGPUImageSoftLightBlendFragmentShaderString = SHADER_STRING
  {
      vec4 base = texture2D(inputImageTexture, textureCoordinate);
      vec4 overlay = texture2D(inputImageTexture2, textureCoordinate2);
+     
+     base.rgb *= base.a;
+     overlay.rgb *= overlay.a;
      
      float alphaDivisor = base.a + step(base.a, 0.0); // Protect against a divide-by-zero blacking out things in the output
      gl_FragColor = base * (overlay.a * (base / alphaDivisor) + (2.0 * overlay * (1.0 - (base / alphaDivisor)))) + overlay * (1.0 - base.a) + base * (1.0 - overlay.a);
