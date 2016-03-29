0412efa7b1e344f2c65080bf44cf2f464b2c76be
Filterd <optic@Steves-MacBook-Pro.local>
2016-03-29 13:20:57 +0100

    .

--- framework/Source/GPUImageLinearLightBlendFilter.m
+++ framework/Source/GPUImageLinearLightBlendFilter.m
@@ -35,9 +35,9 @@ NSString *const kGPUImageLinearLightBlendFragmentShaderString = SHADER_STRING
      highp float ga;
      highp float ba;
      
-   	 ra.r = (base.r > 0.5) ? (overlay.r)+(2.0*(base.r-0.5)) : (base.r +(2.0*overlay.r-1.0));
-     ga.g = (base.g > 0.5) ? (overlay.g)+(2.0*(base.g-0.5)) : (base.g +(2.0*overlay.g-1.0));
-     ba.b = (base.b > 0.5) ? (overlay.b)+(2.0*(base.b-0.5)) : (base.b +(2.0*overlay.b-1.0));
+   	 ra = (base.r > 0.5) ? (overlay.r)+(2.0*(base.r-0.5)) : (base.r +(2.0*overlay.r-1.0));
+     ga = (base.g > 0.5) ? (overlay.g)+(2.0*(base.g-0.5)) : (base.g +(2.0*overlay.g-1.0));
+     ba = (base.b > 0.5) ? (overlay.b)+(2.0*(base.b-0.5)) : (base.b +(2.0*overlay.b-1.0));
      
      //remove the alpha HERE below before the 0.5 if funny
 /*     ra = (base.r > 0.5) ? (overlay.r * overlay.a) + (2.0 * (base.r * base.a - 0.5)) : (overlay.r * overlay.a + (2.0 * base.r * base.a - 1.0));
